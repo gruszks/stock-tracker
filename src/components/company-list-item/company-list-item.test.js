1b052d0cc682dcc401074dcb63793adbe74f4205
Patryk Gruszka <patryk.gruszka@stxnext.pl>
2019-08-11 15:11:57 +0200

    Update react react-dom to 16.9.0. Add unit tests

---
+++ src/components/company-list-item/company-list-item.test.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { render, fireEvent, waitForElement } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+
+import CompanyListItem from './company-list-item';
+
+it('should renders correctly', () => {
+  const company = {
+    change: 1.12,
+    changePercent: 0.589,
+    currency: 'USD',
+    domain: 'facebook.com',
+    image: 'https://logo.clearbit.com/facebook.com',
+    marketClose: '16:00',
+    marketOpen: '09:30',
+    name: 'Facebook Inc.',
+    price: 189.04,
+    region: 'United States',
+    symbol: 'FB',
+    timezone: 'UTC-04',
+    tradingDay: '2019-08-09',
+  };
+  const { asFragment } = render(
+    <CompanyListItem data={company} onDeleteCompany={jest.fn()} />
+  );
+
+  expect(asFragment()).toMatchSnapshot();
+});
